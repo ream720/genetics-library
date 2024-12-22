b7755f48b9dbeb6055724d9d6143a3358fd7b3dd
Zak Ream <ream720@gmail.com>
2024-12-22 17:04:46 -0500

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Use this if deploying to root, or remove entirely if not needed
+  base: "/genetics-library/", // Set the base path
 });
