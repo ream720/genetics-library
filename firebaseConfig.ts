59e445893853e85e82cbb3a2099b6b71d72f00ee
Zak Ream <ream720@gmail.com>
2024-12-22 17:35:50 -0500

    add secrets and env workflow

---
+++ firebaseConfig.ts
@@ -0,0 +1,23 @@
+// firebaseConfig.ts
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics"; // If you are not using analytics you can remove this line
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app); // If you are not using analytics you can remove this line
+
+// Initialize Authentication
+export const auth = getAuth(app);
+export const db = getFirestore(app);
