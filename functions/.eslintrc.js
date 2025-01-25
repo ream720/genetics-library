c823b21795c3ee7940eb0e52b42e829555029dc6
Zak Ream <ream720@gmail.com>
2025-01-25 16:29:22 -0500

    fix build 3

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,21 +1,10 @@
-// @ts-nocheck
-export default {
+module.exports = {
   root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["tsconfig.json"],
-    sourceType: "module",
-  },
   plugins: ["@typescript-eslint"],
-  rules: {
-    quotes: ["error", "single"],
-    "object-curly-spacing": ["error", "always"],
+  env: {
+    node: true,
   },
   ignorePatterns: ["/lib/**/*", "/dist/**/*"],
 };
